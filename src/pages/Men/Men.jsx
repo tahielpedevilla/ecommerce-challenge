c23c1ea10b70fd2c67705a50478bc3746a971935
Tahiel Pedevilla <tahielpedevilladedonato@gmail.com>
2022-04-07 13:11:20 -0300

    test mock routes

--- src/pages/Men/Men.jsx
+++ src/pages/Men/Men.jsx
@@ -6,6 +6,7 @@ import {useProducts} from "@/hooks/useProducts";
 
 const Men = () => {
   const {products} = useProducts();
+
   const filteredProducts = products.filter((product) => product.category === "Men");
 
   return (
