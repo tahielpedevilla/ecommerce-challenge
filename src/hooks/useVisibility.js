ee6fa9f6c24fc78c737cc057f1a0316b344d5022
Tahiel Pedevilla <tahielpedevilladedonato@gmail.com>
2022-04-08 11:33:31 -0300

    delete useVisibility custom hook

--- src/hooks/useVisibility.js
+++
@@ -1,8 +0,0 @@
-import {useState} from "react";
-
-export const useVisibility = () => {
-  const [isVisible, setVisibility] = useState(false);
-  const toggleVisibility = () => setVisibility(!isVisible);
-
-  return {isVisible, toggleVisibility};
-};
